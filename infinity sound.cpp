48b512b9e4ab14d05d1624061cf6ec811df9f410
rukiamuq <kusagaid@gmail.com>
2025-06-13 18:02:07 +0300

    Add files via upload

---
+++ infinity sound.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	//SetConsoleCP(1251), SetConsoleOutputCP(1251);
+	for (;;) {cout << "\a";}
+	return 0;
+
+}
