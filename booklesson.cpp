b5c8d588d854128326c8d2e293fba98edb1ccdee
rukiamuq <kusagaid@gmail.com>
2025-06-16 13:08:29 +0300

    Update booklesson.cpp

--- booklesson.cpp
+++ booklesson.cpp
@@ -1,14 +1,1 @@
-﻿#include <iostream>
-#include <windows.h>
-#include <string>
-using namespace std;
-
-
-int main()
-{
-	setlocale(LC_ALL, "RU");
-	//SetConsoleCP(1251), SetConsoleOutputCP(1251);
-	for (;;) {cout << "\a";}
-	return 0;
-
-}
+Open file Infinity Sound . cpp
